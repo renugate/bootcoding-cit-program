299ca99d3ea4f64921fbacaea78aee8b0476462a
renugate <144773133+renugate@users.noreply.github.com>
2023-09-12 16:02:56 +0530

    Add files via upload

---
+++ Ifelse2.java
@@ -0,0 +1,16 @@
+public class Ifelse2
+{
+public static void main(String args[])
+{
+  //find out the largest number from given three numbers
+int a=20, b=30, c=11;
+if(a>b && a>c){
+System.out.println(a +"is greater");
+}else if(b>a && b>c){
+System.out.println(b +"is greater");
+}else{
+System.out.println(c +"is greater");
+}
+}
+}
+
