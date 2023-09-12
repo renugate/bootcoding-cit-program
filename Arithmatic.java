3659b42807f6ee02f6a6e93bea88b38c983be1ed
renugate <144773133+renugate@users.noreply.github.com>
2023-09-12 16:10:05 +0530

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,12 @@
+public class Arithmatic
+{
+public static void main(String args[])
+{
+int a=10;
+if(a%2==0){
+System.out.println(a +"is an even number");
+}else{
+System.out.println(a +"is an odd number");
+}
+}
+}
---
+++ Ifelse.java
@@ -0,0 +1,12 @@
+public class Ifelse
+{
+public static void main(String args[])
+{
+int a=11;
+if(a%2==0){
+System.out.println(a +"is an even number");
+}else{
+System.out.println(a +"is an odd number");
+}
+}
+}
